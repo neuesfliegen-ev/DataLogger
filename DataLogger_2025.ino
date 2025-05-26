e50339e41410edf438cb747c951f304774cdb484
neuesfliegen-ev <neuesfliegen.ev@gmail.com>
2025-05-26 18:26:44 +0200

    Update DataLogger_2025.ino

--- DataLogger_2025.ino
+++ DataLogger_2025.ino
@@ -10,6 +10,12 @@
 // Create GPS parser
 TinyGPSPlus gps;
 
+
+// Voltage divider values
+const float R1 = 1800.0; // Ohms
+const float R2 = 380.0;  // Ohms
+
+
 // === OLED Setup (SH1106 I2C) ===
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
