7c374f27d2dda2fcf89f56dbc028f51daf791c11
gungie-adam77 <108061355+gungie-adam77@users.noreply.github.com>
2025-06-19 15:48:59 +0200

    Create Test_6.py
    Created Test_6.py to show the same convolution method as in Test_4.py and Test_5.py but with the offset being removed.
    
    A small mistake was also fixed in Test_5.py

--- Python_Filter_Processing/Test_6.py
+++ Python_Filter_Processing/Test_6.py
@@ -3,8 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 """
--This test is to show the effects of using a centered moving average filter with dynamic window size adjustment<
--This is the same concept as Test_4.py; as such I also recommend this
+-This test is to show the effects of using a centered moving average filter with dynamic window size adjustment and removing an offset
+-This is the same concept as Test_4.py and Test_5.py; as such I also recommend this
 -Window size is 100 but can be adjusted to however big is needed. 
 -The bigger the window size, the greater the smoothing
 -Can be improved upon to accept real-time data points
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 """
 
-def centered_moving_average(data, window_size):
+def centered_moving_average(data, window_size, offset):
     half_win = window_size // 2  # e.g., 2 for a 5-point window
     smoothed = np.zeros_like(data) # create a new array of zeros with the same size as input 'data' array
     
@@ -20,18 +20,19 @@ def centered_moving_average(data, window_size):
         # Determine the dynamic window bounds
         start = max(0, i - half_win)          # Avoid negative indices
         end = min(len(data), i + half_win) # Avoid exceeding array length
-        smoothed[i] = np.mean(data[start:end])
+        smoothed[i] = np.mean(data[start:end] - offset)
     
     return smoothed
 
 df = pd.read_excel('Datalogger.xlsx', sheet_name='Sheet2') # Import entire Excel file as a dataframe
 time = df['adjusted_timestamp']
-acc_z = df['accZ']
+acc_z = df['accZ'] - 1
 window_size = 100 # adjust window size here as needed
+offset = 1 # adjust offset here as needed
 
-filtered_acc_z = centered_moving_average(df['accZ'], window_size)
+filtered_acc_z = centered_moving_average(df['accZ'], window_size, offset)
 
-plt.figure(figsize=(12, 6))
+plt.figure(figsize=(20, 15))
 plt.plot(time, acc_z, color='gray', alpha=0.5, label='Raw Data')
 plt.plot(time, filtered_acc_z, color='red', linewidth=2, label=f'Smoothed ({window_size}-point MA)')
 
@@ -43,4 +44,3 @@ plt.legend()
 plt.grid(alpha=0.3)
 plt.tight_layout()  # Prevents label cutoff
 plt.show()
-
