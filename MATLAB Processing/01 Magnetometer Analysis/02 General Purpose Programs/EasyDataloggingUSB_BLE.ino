84b0323a707723543ab08982848c58305f3e6cf7
Luciano Carricart <wzv135@haw-hamburg.de>
2025-08-13 21:39:29 +0200

    Updated simplified application to work with BLE handlers

--- MATLAB Processing/01 Magnetometer Analysis/02 General Purpose Programs/EasyDataloggingUSB_BLE.ino
+++ MATLAB Processing/01 Magnetometer Analysis/02 General Purpose Programs/EasyDataloggingUSB_BLE.ino
@@ -3,10 +3,12 @@
 #include <Wire.h>
 #include <U8g2lib.h>
 #include <Arduino.h>
+#include <ArduinoBLE.h>
 
 #define loggingPeriod 100     // logging Period in milli seconds. Logging Freq= 1/ loggingPeriod KHz
 #define CALIB_SAFETY_CHECK 1  // boolean
-#define CALIB_DEBUG 0
+#define CALIB_DEBUG 1
+#define WITH_BLUETOOTH 1      // (Mechanical / BLE) button
 
 // === Global Sensor Variables ===
 unsigned long timestamp;
@@ -26,6 +28,7 @@ unsigned long lastEdge = 0;
 bool lastStableState = HIGH;            // Assuming pull-up resistor
 
 // === Calibration Variables ===
+float timeOffset;
 float accX_off  = 0, accY_off  = 0, accZ_off  = 0;
 float gyroX_off = 0, gyroY_off = 0, gyroZ_off = 0;
 float magX_off  = 0, magY_off  = 0, magZ_off  = 0;
@@ -38,18 +41,39 @@ bool retryCalibration = 0;
 unsigned long now = 0;
 unsigned long lastCollectTime = 0; 
 
+// ====== LE Bluetooth ======
+/* A BLE UUID is a 128-bit value written in hexadecimal, like: 19B10000-E8F2-537E-4F6C-D104768A1214
+   This format is standardized by the ITU (International Telecommunication Union).
+   The idea is that the probability of two people picking the same UUID by accident is essentially zero. 
+*/
+BLEService svc("19B10000-E8F2-537E-4F6C-D104768A1214");                     // svc UUID — Universally Unique Identifier
+BLEByteCharacteristic triggerChar( "19B10001-E8F2-537E-4F6C-D104768A1214",  // triggerChar UUID, not the same: "0001"
+                                    BLEWrite | BLEWriteWithoutResponse
+);
+BLEDevice central;                          // Will contrain information about my central (phone)
+uint8_t v = 0;                              // The message I am sending from my central
+volatile bool triggerRequested = false;     // volatile variables to be used in Even Handlers. An event handler can be interrupted by an ISR I guess, so keep (atomic) 1 byte size data type like "bool"
+volatile bool bleConnected = false;
+
 // === Function Declarations ===
 void updateIMUData();
 void generateDataLine();
 void calibrateIMU(); 
 void calibrateIMU2();
 void waitForButtonPressed();
-void onButtonPress();
+void onButtonPress();       // ISR
+void onTriggerWritten(BLEDevice, BLECharacteristic);    // Event Handler (RTOS). BLEDevice and BLECharacteristics are the data types
 bool fullSpanCalibration(int16_t, int16_t, int16_t, int16_t, int16_t, int16_t);
+void initializeBLE();
 
 void setup() {
   Serial.begin(9600);    // USB serial for debug
   analogReadResolution(12); 
+
+  // BLE
+  if (WITH_BLUETOOTH == 1) {
+    initializeBLE();
+  }
 
   // Button 
   pinMode(PIN_BUTTON, INPUT_PULLUP);
@@ -98,6 +122,7 @@ void setup() {
   // Wait for button to start logging
   bool toggle = false;
   waitForButtonPressed();
+  timeOffset = millis();
 }
 
 void loop() {
@@ -111,6 +136,22 @@ void loop() {
   }
   // Optional: add a short delay to reduce CPU load if needed
   // delay(1000); // (uncomment if display flickers or CPU usage is high)
+}
+
+// === BLE Inititialization ===
+void initializeBLE() {
+  while (!BLE.begin()) { yield(); }     // Make sure the Bluetooth module starts
+  BLE.setLocalName("NFC25-Datalogger");
+  BLE.setAdvertisedService(svc);        // This call tells ArduinoBLE: include this service’s UUID in the advertising packets. Why? So that a central (phone) can filter scans for devices that have a specific service without connecting first.
+  svc.addCharacteristic(triggerChar);   // Actual data/command. One service can have multiple characteristics
+  BLE.addService(svc);
+
+  // Set Event Handlers, like ISR but not hardware driven, involved in the RTOS
+  triggerChar.setEventHandler(BLEWritten, onTriggerWritten);
+  BLE.setEventHandler(BLEConnected, onConnect);
+  BLE.setEventHandler(BLEDisconnected, onDisconnect);
+
+  BLE.advertise();                      // Starts advertising: sending out periodic BLE “I'm here” packets
 }
 
 // === IMU Data Update ===
@@ -160,23 +201,64 @@ void onButtonPress() {
   }
 }
 
-void waitForButtonPressed() {
-  while(!buttonInterruptFlag) {
-    yield();    // Nano BLE 33, RTOS native instructions. Doesn't starve the processor with delay(500) and ensures proper waiting of the button pressed (even though our current button doesnt have that problem).
-  }
+/* Event Handler, RTOS functionality. Works like an ISR but not hardware driven.
+   The parameters "central" and "characteristic" seem unused but are crucial */
+void onTriggerWritten(BLEDevice central, BLECharacteristic characteristic) {
+  v = 0;
+  triggerChar.readValue(v);
 
-  buttonInterruptFlag = false; 
+  if (v == 0x01) {
+    triggerRequested = true;
+  }
+}
+
+// Event Handler, BLE
+void onConnect(BLEDevice central) {
+  bleConnected = true;      
+  central = BLE.central();       
+  // Now we can operate methods for this object Central (my phone).   
+  // It updates because different centrals (phones) could connect at different stages of the program 
+}
+
+// Event Handler, BLE
+void onDisconnect(BLEDevice central) {
+  bleConnected = false;
+  BLE.advertise();                   
+}
+
+void waitForButtonPressed() {
+  if (WITH_BLUETOOTH == 1) {
+    while (!triggerRequested) {
+      yield();
+      BLE.poll();   
+      // let BLE stack run; otherwise it starves. This is because yield() lets the scheduler loop run, but the stack isnt integrated thereand it can't run
+    }
+
+    v = 0;
+    triggerRequested = false;
+    } else if (WITH_BLUETOOTH == 0) {
+      while(!buttonInterruptFlag) {
+        yield();
+      }
+      buttonInterruptFlag = false; 
+    }
 }
 
 void generateDataLine() {
   String line = "";
 
   if (first_writing == 0) {    
-    Serial.println("\nmagX,magY,magZ");
+    Serial.println("\ntime,accX,accy,accY,gyroX,gyroY,gyroZ,magX,magY,magZ");
     line = "";
     first_writing++;
   }
-
+  line += String(millis() - timeOffset) + ",";
+  line += String(accX) + ",";
+  line += String(accY) + ",";
+  line += String(accZ) + ",";
+  line += String(gyroX) + ",";
+  line += String(gyroY) + ",";
+  line += String(gyroZ) + ",";
   line += String(magX) + ",";
   line += String(magY) + ",";
   line += String(magZ);
@@ -200,6 +282,7 @@ void calibrateIMU() {
     // Waits until all data is ready for collection
     while (!IMU.accelerationAvailable() || !IMU.gyroscopeAvailable()) {
       yield();
+      BLE.poll();
     }
 
     IMU.readAcceleration(accX, accY, accZ);
@@ -275,11 +358,13 @@ void calibrateIMU2() {
 
   int j = 0;
   buttonInterruptFlag = false;
-  while(!buttonInterruptFlag && j < MAGNET_CALIB_SAMPLES) {
+  triggerRequested = false;
+  while(!buttonInterruptFlag && !triggerRequested && j < MAGNET_CALIB_SAMPLES) {
     while (!IMU.magneticFieldAvailable()) {
       yield();
+      BLE.poll();
     }
-
+    
     IMU.readMagneticField(magX, magY, magZ);
 
     magnXCalibBuffer[j] = magX;  magnYCalibBuffer[j] = magY; magnZCalibBuffer[j] = magZ;
@@ -314,16 +399,16 @@ void calibrateIMU2() {
 
   // Calibration reliability check-up
   if (CALIB_SAFETY_CHECK == 1) {
-    // If calibration lasted less than 30'' or there's a lack of positive || negative values recorded, then the calibration isn't reliable
-    if (j < 600 || fullSpanCalibration(xMin, xMax, yMin, yMax, zMin, zMax)) {
+    // If calibration lasted less than 15'' or there's a lack of positive || negative values recorded, then the calibration isn't reliable
+    if (j < 150 || fullSpanCalibration(xMin, xMax, yMin, yMax, zMin, zMax)) {
       retryCalibration = 1;
-    }
+    } else { retryCalibration = 0; }
   }
 } // buffers go out of scope here, RAM reclaimed automatically
 
 // This helper function checks that the recorded range sits on 50% of the expected span range. Otherwise, the calibration isn't reliable.
 bool fullSpanCalibration(int16_t xMin, int16_t xMax, int16_t yMin, int16_t yMax, int16_t zMin, int16_t zMax) {
-  if (((xMax - xMin) < 20) || ((yMax - yMin) < 50) || ((zMax - zMin) < 50)) {
+  if (((xMax - xMin) < 45) || ((yMax - yMin) < 50) || ((zMax - zMin) < 50)) {
     return 1;
   } else {
     return 0;
